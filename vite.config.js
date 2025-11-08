dba189a5678c9fc6e4b52e774c1a20f1c52ca034
scaradook13 <mythicalwarrior132006@gmail.com>
2025-11-09 03:09:39 +0800

    fix vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,4 +16,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  },
+  // 👇 This ensures correct routing behavior on Vercel or any static host
+  base: '/',
 })
